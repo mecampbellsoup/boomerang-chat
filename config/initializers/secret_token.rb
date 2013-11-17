51c168594fd839e613ea3a2c48a5ef35a3050e96
Matt Campbell <mecampbell25@gmail.com>
2013-11-17 13:58:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BoomerangChat::Application.config.secret_key_base = '3b2834526b8270263164d55ee69444058b201b9d322a3691ce76b380cf57a9b818649993da8874e714cf0111db2b321f11b72c0d83f00c9a39faf98268b353c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BoomerangChat::Application.config.session_store :cookie_store, key: '_boomerang-chat_session'
